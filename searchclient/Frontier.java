4d67f3bbe03e0bd20e1860d38e613b11df0f1e84
blackbear <qyq1226@gmail.com>
2025-02-09 12:30:40 +0100

    init

---
+++ searchclient/Frontier.java
@@ -0,0 +1,136 @@
+package searchclient;
+
+import java.nio.file.attribute.UserDefinedFileAttributeView;
+import java.util.ArrayDeque;
+import java.util.HashSet;
+
+public interface Frontier {
+    void add(State state);
+
+    State pop();
+
+    boolean isEmpty();
+
+    int size();
+
+    boolean contains(State state);
+
+    String getName();
+}
+
+class FrontierBFS
+        implements Frontier {
+    private final ArrayDeque<State> queue = new ArrayDeque<>(65536);
+    private final HashSet<State> set = new HashSet<>(65536);
+
+    @Override
+    public void add(State state) {
+        this.queue.addLast(state);
+        this.set.add(state);
+    }
+
+    @Override
+    public State pop() {
+        State state = this.queue.pollFirst();
+        this.set.remove(state);
+        return state;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.queue.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return this.queue.size();
+    }
+
+    @Override
+    public boolean contains(State state) {
+        return this.set.contains(state);
+    }
+
+    @Override
+    public String getName() {
+        return "breadth-first search";
+    }
+}
+
+class FrontierDFS
+        implements Frontier {
+    private final ArrayDeque<State> queue = new ArrayDeque<>(65536);
+    private final HashSet<State> set = new HashSet<>(65536);
+
+    @Override
+    public void add(State state) {
+        this.queue.add(state);
+        this.set.add(state);
+    }
+
+    @Override
+    public State pop() {
+        State state = this.queue.pollLast();
+        this.set.remove(state);
+        return state;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.queue.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return this.queue.size();
+    }
+
+    @Override
+    public boolean contains(State state) {
+        return this.set.contains(state);
+    }
+
+    @Override
+    public String getName() {
+        return "depth-first search";
+    }
+}
+
+class FrontierBestFirst
+        implements Frontier {
+    private Heuristic heuristic;
+
+    public FrontierBestFirst(Heuristic h) {
+        this.heuristic = h;
+    }
+
+    @Override
+    public void add(State state) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public State pop() {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public int size() {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public boolean contains(State state) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public String getName() {
+        return String.format("best-first search using %s", this.heuristic.toString());
+    }
+}
